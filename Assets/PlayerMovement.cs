e6fb224e612ef7cf18a0727f6b84db26ae699088
Duy Phung <duypv98@gmail.com>
2020-04-05 00:06:29 +0700

    add script component for object. demo rigidbody

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;*/
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Reference to the Rigidbody component called "rb"
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    /*void Start() {
+        // Debug.Log("Hello, World !");
+        // rb.useGravity = false;
+        rb.AddForce(0, 200, 500);
+    }*/
+
+    // Update is called once per frame
+    // FixedUpdate: using to mess with physics
+    void FixedUpdate() {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
