25742956f0e5e56b6bf8792ab9fce80be1797cd3
Duy Phung <duypv98@gmail.com>
2020-04-12 18:01:54 +0700

    videos05: basic handling of collision in Unity

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement Movement;
+
+    void OnCollisionEnter(Collision collision) {
+
+        /* Debug.Log(collision.collider.name); */
+        if (collision.collider.tag == "Obstacle") {
+            Debug.Log("We hit an obstacle !");
+            Movement.enabled = false;
+        }
+    }
+
+}
