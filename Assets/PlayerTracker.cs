b3ef6410cabc05e1a6af972c26aac6624a60d8d8
Duy Phung <duypv98@gmail.com>
2020-04-12 12:53:51 +0700

    videos4: Camera Follow

---
+++ Assets/PlayerTracker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerTracker : MonoBehaviour {
+    // References to the Player
+    public Transform Player;
+    public Vector3 Offset;
+
+    // Update is called once per frame
+    void Update() {
+        /* Debug.Log(Player.position); */
+        /* this.transform.position = Player.position // FPP */
+        this.transform.position = Player.position + Offset; // TPP
+        /* This script is attached with the Main Camera, so that `this` is refers to the Main Camera.
+        Actually, we can just write `transfrom` instead of `this.transform` */
+    }
+}
