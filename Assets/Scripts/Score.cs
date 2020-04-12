89aa931f8f02851cda0e05198d7bc2a7284885ee
Duy Phung <duypv98@gmail.com>
2020-04-13 00:45:57 +0700

    videos07: add UI and score

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public Transform Player;
+    public Text ScoreText;
+
+    // Update is called once per frame
+    void Update() {
+        /* Debug.Log(Player.position.z); */
+        ScoreText.text = "Score: " + (Player.position.z + 45.0).ToString("0");
+    }
+}
