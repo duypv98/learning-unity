4ca3048c7c0a8861a0fd971aa7149ffd70e01204
Duy Phung <duypv98@gmail.com>
2020-04-18 22:19:21 +0700

    v10: export game windows

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour {
+    public void QuitGame() {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+    public void PlayAgain() {
+        SceneManager.LoadScene("Level01");
+    }
+}
